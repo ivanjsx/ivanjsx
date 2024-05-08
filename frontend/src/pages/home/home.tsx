4a8e585974df8b281a27b8eb3930a72f24815858
Ivan Shanin <ivanxshanin@gmail.com>
2024-05-08 16:46:12 +0200

    populate intro section

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,10 +1,22 @@
 // libraries 
 import { FC } from "react";
 
+// styles 
+import styles from "./home.module.css";
+
 
 
 const HomePage: FC = () => {
-  return null;
+  return (
+    <main>
+      <section className={styles.intro}>
+        <p className={styles.text}>
+          I'm Ivan Shanin, a full-stack web developer based in Barcelona.
+          Below are some of the things I've built.
+        </p>
+      </section>
+    </main>
+  );
 }
 
 export default HomePage;
