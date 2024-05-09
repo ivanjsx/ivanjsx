4efeb1a002dea685ced8c213871c165adc901dda
Ivan Shanin <ivanxshanin@gmail.com>
2024-05-09 23:58:18 +0200

    restructure home page

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,22 +1,45 @@
 // libraries 
-import { FC } from "react";
+import { FC, RefObject, useMemo, useRef } from "react";
 
 // styles 
 import styles from "./home.module.css";
 
+// components 
+import Intro from "../../components/intro/intro";
+import ProjectGallery from "../../components/project-gallery/project-gallery";
+
 
 
 const HomePage: FC = () => {
+  
+  const backendProjectsRef = useRef<HTMLDivElement>(null);
+  const frontendProjectsRef = useRef<HTMLDivElement>(null);
+  
+  const scrollIntoSection = useMemo<(sectionRef: RefObject<HTMLDivElement>) => () => void>(
+    () => (sectionRef: RefObject<HTMLDivElement>) => () => {
+      sectionRef.current!.scrollIntoView({ behavior: "smooth" });
+    },
+    []
+  );  
+  
   return (
-    <main>
-      <section className={styles.intro}>
-        <p className={styles.text}>
-          I'm Ivan Shanin, a full-stack web developer based in Barcelona.
-          Below are some of the things I've built.
-        </p>
-      </section>
+    <main className={styles.container}>
+      <Intro
+        scrollTo={frontendProjectsRef}
+        scrollHandler={scrollIntoSection}
+      />
+      <ProjectGallery 
+        ref={frontendProjectsRef}
+        stack="frontend"
+        title="Selected Frontend Projects" 
+      />
+      <ProjectGallery 
+        ref={backendProjectsRef}
+        stack="backend"
+        title="Selected Backend Projects" 
+      />
     </main>
   );
-}
+};
 
 export default HomePage;
